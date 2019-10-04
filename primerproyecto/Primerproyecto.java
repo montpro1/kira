ed516abbd1a8af0f12bb6b7d349b1b452bb9f851
Víctor Enrique Montes Tovar <vmontestovar@nautilus19.ad.danielcastelao.org>
2019-10-04 14:36:30 +0200

    escribi mas

--- primerproyecto/Primerproyecto.java
+++ primerproyecto/Primerproyecto.java
@@ -10,13 +10,17 @@ package primerproyecto;
  * @author vmontestovar
  */
 public class Primerproyecto {
+    //que poasa//
+   
+    
+    
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hello"
-                );// TODO code application logic here
+        System.out.println("hello worlodo");
+                // TODO code application logic here
     }
     
 }
