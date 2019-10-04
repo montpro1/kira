5175f9a52fab51847a929770a925fe0bf31ec5da
Víctor Enrique Montes Tovar <vmontestovar@nautilus19.ad.danielcastelao.org>
2019-10-04 14:06:07 +0200

    Inicio

---
+++ primerproyecto/Primerproyecto.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primerproyecto;
+
+/**
+ *
+ * @author vmontestovar
+ */
+public class Primerproyecto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hello"
+                );// TODO code application logic here
+    }
+    
+}
